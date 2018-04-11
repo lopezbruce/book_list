20ffc7a27e608babb97923ebfa878ec30351b77f
blopez6 <blopez6@uncc.edu>
2018-04-11 10:03:24 -0400

    finished promoting booklist to a container

---
+++ src/containers/book-list.js
@@ -0,0 +1,19 @@
+import React, { Compenont } from 'react';
+
+export default class BookList extends Compenont {
+  renderList () {
+    return this.props.books.map((book) => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render () {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
